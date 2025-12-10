82c2ef5a4f78e671b37ed83daa2a9bd97efd8db2
Quentin Donnelly <quentindonnelly91@gmail.com>
2025-12-10 14:11:57 -0500

    add path utils module with function to sanity check / normalize model path

---
+++ utils/path_utils.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+def get_model_path(model: str) -> Path:
+    """
+    Returns the path to the model directory.
+    """
+    ROOT = Path.cwd()
+    if not (ROOT / "models").exists():
+        # We're probably in notebooks/, go one level up
+        ROOT = ROOT.parent
+
+    print("Project root assumed as:", ROOT)
+    print("Models dir exists:", (ROOT / "models").exists())
+
+    model_path = ROOT / "models" / model
+    return model_path
